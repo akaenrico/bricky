c34aab378e13abe4db89f20ca2c39d86fb8c7b11
Enrico Brosque Palma <enricob.palma@gmail.com>
2025-03-11 21:12:32 -0300

    Fixes User Model namespace

--- aspnet-backend/aspnet-backend/Models/User.cs
+++ aspnet-backend/aspnet-backend/Models/User.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Bricky.Models;
+namespace aspnet_backend.Models;
 
 public class User
 {
